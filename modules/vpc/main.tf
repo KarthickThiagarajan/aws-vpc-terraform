48949c3099cb4da60624ffbf5b502d0c273c3802
Karthick <92004529+KarthickThiagarajan@users.noreply.github.com>
2026-09-19 10:58:07 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,3 +17,31 @@ resource "aws_internet_gateway" "this" {
     Environment = var.environment
   }
 }
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.name}-public-${count.index + 1}"
+    Environment = var.environment
+    Tier        = "Public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name        = "${var.name}-private-${count.index + 1}"
+    Environment = var.environment
+    Tier        = "Private"
+  }
+}
