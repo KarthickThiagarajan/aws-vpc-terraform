7ffb1b3945abfd5a9df0d30bdb4b22295f7cbd54
Karthick <92004529+KarthickThiagarajan@users.noreply.github.com>
2026-09-21 06:15:01 -0400

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,7 +8,6 @@ resource "aws_vpc" "this" {
     Environment = var.environment
   }
 }
-
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
@@ -31,7 +30,6 @@ resource "aws_subnet" "public" {
     Tier        = "Public"
   }
 }
-
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
 
@@ -54,7 +52,6 @@ resource "aws_eip" "nat" {
     Environment = var.environment
   }
 }
-
 resource "aws_nat_gateway" "this" {
   count = length(var.availability_zones)
 
@@ -69,4 +66,45 @@ resource "aws_nat_gateway" "this" {
     Name        = "${var.name}-nat-${count.index + 1}"
     Environment = var.environment
   }
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name        = "${var.name}-public-rt"
+    Environment = var.environment
+    Tier        = "Public"
+  }
+}
+resource "aws_route_table" "private" {
+  count  = length(var.availability_zones)
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = {
+    Name        = "${var.name}-private-rt-${count.index + 1}"
+    Environment = var.environment
+    Tier        = "Private"
+  }
+}
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
